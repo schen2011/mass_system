b24d38594f474124db6759224385e396223b42af
schen2011 <schenjobs@gmail.com>
2018-04-14 23:24:53 -0400

    update property file

--- src/main/resources/data-h2.sql
+++
@@ -1,2 +0,0 @@
-insert into admin_users
-values('Mythili','ABC');
--- src/main/resources/schema-h2.sql
+++
@@ -1,6 +0,0 @@
-create table admin_users
-(
-   name varchar(255) not null,
-   password varchar(255) not null,
-   primary key(name)
-);
