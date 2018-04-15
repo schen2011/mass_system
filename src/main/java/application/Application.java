2bd41f72b422d5e60115c8da6608167c659d10ee
mythi <mythi@DESKTOP-0OA27MT>
2018-04-14 22:38:35 -0400

    Base setup of H2 with spring boot

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -2,8 +2,11 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/resources/schema-h2.sql
@@ -0,0 +1,6 @@
+create table admin_users
+(
+   name varchar(255) not null,
+   password varchar(255) not null,
+   primary key(name)
+);
